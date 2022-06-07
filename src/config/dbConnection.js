3ccc1fdbe9b9ed98f8c98ca0491c65319ade6155
Ochirjav Jargalsaikhan <ochirjavjargalsaikhan@Ochirjavs-MacBook-Air.local>
2022-06-07 23:30:03 +0800

    MongoDB Connection Update

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -8,6 +8,7 @@ async function connectDB() {
                 console.log("\x1b[36m%s\x1b[0m", `MongoDB connected: ${db.connection.host}`);
             });
     } catch (err) {
+        console.log("MongoDB connection unsuccessful");
         console.error(err);
         process.exit(1);
     }
